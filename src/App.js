da42207e142f82f38f1f3bfbe4e58df17d0fcc4a
Luis <luis.felipersi@gmail.com>
2019-11-22 22:41:30 -0300

    retorno dos filmes com axios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 
 class App extends Component {
 constructor(props){
   super(props);
 
   this.state = {
-    filmes: [],
+    films: [],
   }
+  this.getFilms = this.getFilms.bind(this);
 }
+
+getFilms(){
+   return Axios.get('https://swapi.co/api/films/')
+   .then((response) => {
+    console.log(response.data.results);
+    this.setState( { films: response.data.results } )
+    })
+}
+
+componentDidMount(){
+  this.getFilms();
+}
+
 render(){
   return (
     <div className="App">
